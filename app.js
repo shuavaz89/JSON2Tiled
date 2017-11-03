692cd6f4a02490c110e60b1587d71cc8fe70fa30
Joshua Vazquez <jovazquez@deloitte.com>
2017-11-03 16:18:29 -0400

    Will download a zip file if there are multiple uploads.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var path = require('path');
+var fs = require('fs');
+var archiver = require('archiver');
 var multer = require('multer')
 var upload = multer()
 var app = express()
 var json2tiled = require('./lib/json2tiled');
+var zip;
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -15,6 +18,15 @@ app.post('/upload', upload.array('upload_file'), function (req, res, next) {
         return res.end('<p>Missing file  <a href="/">Back</a></p>');
     }
 
+    // If multiple files setup zip
+    if (req.files.length > 1) {
+        zip = archiver('zip');
+        res.set('Content-disposition', 'attachment; filename=Tiled.zip');
+        res.set('Content-Type', 'application/zip');
+        zip.pipe(res);
+    }
+
+    //Iterate uploaded JSON files
     for (var i = 0, len = req.files.length; i < len; i++) {
         var file = req.files[i];
         var fileInfo = path.parse(file.originalname);
@@ -26,9 +38,22 @@ app.post('/upload', upload.array('upload_file'), function (req, res, next) {
         //Setup XML
         var xml = json2tiled(JSON.parse(file.buffer))
 
-        res.set('Content-disposition', 'attachment; filename=' + fileInfo.name + '.tmx');
-        res.set('Content-Type', 'application/xml');
-        res.send(xml);
+        if (req.files.length === 1) {
+            //Download tmx tile
+            res.set('Content-disposition', 'attachment; filename=' + fileInfo.name + '.tmx');
+            res.set('Content-Type', 'application/xml');
+            res.send(xml);
+        } else {
+            //Write xml file to temporary directory
+            zip.append(xml, {
+                name: fileInfo.name + '.tmx'
+            })
+        }
+    }
+
+    // Finalize zip
+    if (req.files.length > 1) {
+        zip.finalize();
     }
 })
 
