228e8e9dc5884cb8579ba080208468be8f460158
Joshua Vazquez <jovazquez@deloitte.com>
2017-11-03 15:29:15 -0400

    Fixed a bug

--- app.js
+++ app.js
@@ -1,43 +1,35 @@
 var express = require('express')
 var path = require('path');
 var multer = require('multer')
-var builder = require('xmlbuilder');
 var upload = multer()
 var app = express()
-var json2tiled = require('json2tiled');
-
-function isEmpty(obj) {
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop))
-            return false;
-    }
-
-    return JSON.stringify(obj) === JSON.stringify({});
-}
+var json2tiled = require('./lib/json2tiled');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.post('/upload', upload.single('upload_file'), function (req, res, next) {
+app.post('/upload', upload.array('upload_file'), function (req, res, next) {
 
-    if (!req.file) {
+    if (req.files.length === 0) {
         return res.end('<p>Missing file  <a href="/">Back</a></p>');
     }
 
-    var fileInfo = path.parse(req.file.originalname);
+    for (var i = 0, len = req.files.length; i < len; i++) {
+        var file = req.files[i];
+        var fileInfo = path.parse(file.originalname);
 
-    if (fileInfo.ext !== '.json') {
-        return res.end('<p>File format must be JSON <a href="/">Back</a></p>');
+        if (fileInfo.ext !== '.json') {
+            return res.end('<p>File format must be JSON <a href="/">Back</a></p>');
+        }
+
+        //Setup XML
+        var xml = json2tiled(JSON.parse(file.buffer))
+
+        res.set('Content-disposition', 'attachment; filename=' + fileInfo.name + '.tmx');
+        res.set('Content-Type', 'application/xml');
+        res.send(xml);
     }
-
-    //Setup XML
-    var xml = json2tiled(JSON.parse(req.file.buffer))
-
-    res.set('Content-disposition', 'attachment; filename=Tiled.tmx');
-    res.set('Content-Type', 'application/xml');
-    res.send(xml);
-
 })
 
 app.listen(3000, function () {
